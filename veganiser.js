f9aae45841dc33da993ec0a638adb5046903e367
chrismact <81088775+chrismact@users.noreply.github.com>
2021-09-21 18:52:08 +0200

    fixed a small issue

--- veganiser.js
+++ veganiser.js
@@ -1,11 +1,17 @@
-const badToGood = {
-  milk: "oat milk",
-  butter: "vegan butter",
-  egg: "flax egg",
-  mince: "soy mince",
-};
+// const badToGood = {
+//   milk: "oat milk",
+//   butter: "vegan butter",
+//   egg: "flax egg",
+//   mince: "soy mince",
+// };
 
 function run() {
+  const badToGood = {
+    milk: "oat milk",
+    butter: "vegan butter",
+    egg: "flax egg",
+    mince: "soy mince",
+  };
   function replace(body, from, to) {
     if (body.childNodes.length) {
       body.childNodes.forEach((child) => replace(child, from, to));
