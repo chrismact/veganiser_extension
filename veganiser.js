eaee2f8064b261b4a69ba47468851523834f24b9
chrismact <81088775+chrismact@users.noreply.github.com>
2021-09-21 19:49:29 +0200

    made a few changes to make more sense

--- veganiser.js
+++ veganiser.js
@@ -1,16 +1,25 @@
-// const badToGood = {
-//   milk: "oat milk",
-//   butter: "vegan butter",
-//   egg: "flax egg",
-//   mince: "soy mince",
-// };
-
 function run() {
   const badToGood = {
     milk: "oat milk",
-    butter: "vegan butter",
-    egg: "flax egg",
+    "egg ": "flax egg",
     mince: "soy mince",
+    "minced beef": "soy mince",
+    chicken: "marinated tofu",
+    lamb: "marinated tofu",
+    pork: "marinated tofu",
+    veal: "marinated tofu",
+    meat: "meat-free ",
+    cheese: "dairy-free cheese",
+    "sour cream": "oat cream",
+    "creme fraiche": "oat cream",
+    con: "sin",
+    beef: "seitan",
+    yoghurt: "soya yogurt",
+    yogurt: "soya yogurt",
+    cream: "oat cream",
+    mozzarella: "vegan mozarella",
+    dairy: "dairy-free",
+    butter: "dairy-free butter",
   };
   function replace(body, from, to) {
     if (body.childNodes.length) {
